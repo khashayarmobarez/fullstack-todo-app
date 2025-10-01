6a94d536938d7f266f84c90fba4369ef6b959bbd
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-01 18:42:52 +0330

    Add react-icons dependency and implement layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const layout = () => {
+    return (
+        <div className="container">
+
+            <header className="header">
+                <h1>khashayar Todo App</h1>
+            </header>
+            
+            <div className='container--main'>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default layout;
