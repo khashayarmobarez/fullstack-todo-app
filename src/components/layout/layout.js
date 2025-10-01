4ce1e9413aa2181b937f67978772ec28b59883b1
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-01 18:50:59 +0330

    Refactor layout component to improve structure and add a welcome message

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,14 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
 
-const layout = () => {
+const Layout = ({children}) => {
     return (
         <div className="container">
 
             <header className="header">
                 <h1>khashayar Todo App</h1>
             </header>
-            
+
             <div className='container--main'>
+
+                <aside>
+                    <p>Welcome 👋</p>
+                    <ul>
+                        <li>
+                            <Link href="/">to dos</Link>
+                        </li>
+                    </ul>
+                </aside>
+
+                <section>
+                    {children}
+                </section>
 
             </div>
             
@@ -16,4 +30,4 @@ const layout = () => {
     );
 };
 
-export default layout;
+export default Layout;
