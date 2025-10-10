5090fb9bb223383def6a17afefef0cb5dea99c27
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-10 22:20:12 +0330

    creating sign up page

---
+++ src/app/signUp/page.js
@@ -0,0 +1,10 @@
+import SignupPage from '@/components/template/SignupPage'
+import React from 'react'
+
+function page() {
+  return (
+    <SignupPage />
+  )
+}
+
+export default page
---
+++ src/components/template/SignupPage.js
@@ -0,0 +1,11 @@
+
+
+function SignupPage() {
+  return (
+    <div className="signin-form">
+      <h3>Registration</h3>
+    </div>
+  )
+}
+
+export default SignupPage
