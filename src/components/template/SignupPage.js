d74941c03ae7e678ecc48d4dc87deaac67da6311
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-11 19:57:41 +0330

    Refactor SignupPage component to manage email and password state

--- src/components/template/SignupPage.js
+++ src/components/template/SignupPage.js
@@ -1,12 +1,18 @@
+'use client'
 import Link from "next/link"
+import { useState } from "react"
 
 
 function SignupPage() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="signin-form">
       <h3>Registration form</h3>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
+      <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
       <button>register</button>
       <div>
         <p>Have an account?</p>
