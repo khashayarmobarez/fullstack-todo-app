7e3edfcd60251d1ae08cbeb4828cae8a64f63d4c
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-11 19:51:34 +0330

    Update SignupPage component with registration form elements

--- src/components/template/SignupPage.js
+++ src/components/template/SignupPage.js
@@ -3,7 +3,10 @@
 function SignupPage() {
   return (
     <div className="signin-form">
-      <h3>Registration</h3>
+      <h3>Registration form</h3>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>register</button>
     </div>
   )
 }
