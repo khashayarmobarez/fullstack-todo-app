9bd5262b380cd59ff59aec419f4e5bff10a935ed
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-11 19:59:55 +0330

    Implement sign-up handler in SignupPage component

--- src/components/template/SignupPage.js
+++ src/components/template/SignupPage.js
@@ -8,12 +8,16 @@ function SignupPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const signUpHandler = async () => {
+    const res = await fetch('/api/auth/signup')
+  }
+
   return (
     <div className="signin-form">
       <h3>Registration form</h3>
       <input value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
       <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
-      <button>register</button>
+      <button onClick={signUpHandler}>register</button>
       <div>
         <p>Have an account?</p>
         <Link href='/signin'>Sign in</Link>
