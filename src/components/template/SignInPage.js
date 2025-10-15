43d7066c948e95b24d5f6e62910680e027b86552
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-15 13:45:16 +0330

    devlopment of sign in page part 2

--- src/components/template/SignInPage.js
+++ src/components/template/SignInPage.js
@@ -1,8 +1,16 @@
+import Link from "next/link";
 
 const SignInPage = () => {
     return (
-        <div>
-            
+        <div className="signin-form">
+            <h3>Registration form</h3>
+            <input value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
+            <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+            <button onClick={signUpHandler}>register</button>
+            <div>
+                <p>Have an account?</p>
+                <Link href='/signin'>Sign in</Link>
+            </div>
         </div>
     );
 };
