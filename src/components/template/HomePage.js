83fa0b832b622b0675a323dea9f50c04186e6b64
khashayar <khashayarmobarezbusiness@gmail.com>
2025-10-28 19:57:01 +0330

    Refactor HomePage component to include useEffect for fetching ToDos on mount

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -1,9 +1,23 @@
-import { useState } from "react"
+'use client'
+import { useEffect, useState } from "react"
 
 
 function HomePage() {
 
     const [toDos, setToDos] = useState([])
+
+    useEffect(() => {
+        fetchToDos()
+    } ,[])
+
+    const fetchToDos = async () => {
+        const res = await fetch('/api/toDos')
+        const data = await res.json()
+        console.log("Fetched ToDos:", data)
+        if(data.status === 'success'){
+            setToDos(data.toDos)
+        }
+    }
 
     return (
         <div>
