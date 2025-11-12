045bd9cff7f6608b47ee1571b77a669ccba37a10
khashayar <khashayarmobarezbusiness@gmail.com>
2025-11-12 17:39:50 +0330

    Enhance layout component with sign-out functionality and update README for course reference

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,31 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
 import { VscListSelection } from "react-icons/vsc";
 import { IoAdd } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
+import { signOut, useSession } from 'next-auth/react';
+import { FiLogOut } from 'react-icons/fi';
 
 const Layout = ({children}) => {
+
+    const { status } = useSession();
+
+    const logOutHandler = async() => {
+        signOut();
+    }
+
+
     return (
         <div className="container">
 
             <header className="header">
                 <h1>Test App</h1>
+                { status === 'authenticated' &&
+                    <button onClick={() => logOutHandler()}>
+                        Sign Out<FiLogOut />
+                    </button>
+                }
             </header>
 
             <div className='container--main'>
