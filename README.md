e66ea8fff339cea81d2da63f0214fe786530ea9f
khashayar <khashayarmobarezbusiness@gmail.com>
2025-11-13 16:51:28 +0330

    until the trainings

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-<!-- until 242 in courses -->
+<!-- until 243 in courses -->
 
 ## Getting Started
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
 import HomePage from "@/components/template/HomePage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/signIn');
+  }
+
   return (
     <HomePage />
   );
